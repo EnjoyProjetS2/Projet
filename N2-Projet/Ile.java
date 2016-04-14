11e69394432c4c291e9c55a88e693cfbf6946746
ArthurMartello <arthurmartello@outlook.com>
2016-04-14 18:56:46 +0200

    Semblant de tour par tour
    - modif du while() dans le jeu pour créer un mode équipe par équipe.
    - ajout de popups pour les messages
    
    -> La première équipe à jouer est aléatoire
    -> tant que l'équipe n'a pas fait une action elle doit recommencer
    -> on ne peut pas cliquer sur un perso / navire ennemi 
    
    Bugs:
    - toujours le double clique qui fait chier, des fois faut cliquer deux fois, des fois non... si quelqu'un peut regarder
    - surement lié au double clique mais des fois c'est la mauvaise pop-up qui vient (c'est la popup précédente je crois) je pense que c'est une histoire de coordonnées (clicX/Y) qui se réinitialisent pas
    - des fois le tour d'un équipe est zappé (wtf)

---
+++ N2-Projet/Ile.java
@@ -0,0 +1,437 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class Ile {
+
+	private Parcelle[][] grille;
+	private int ligne;
+	private int colonne;
+	private double tauxRocher;
+	private int posNav1;
+	private int posNav2;
+
+	/**
+	 * Constructeur par defaut : Cree une ile vide avec des parcelles
+	 * 
+	 * @param Ile
+	 */
+	public Ile() { // ile vide sans navires
+
+		this.grille = new Parcelle[ligne][colonne];
+		this.tauxRocher = 0;
+
+		ileVierge();
+	}
+
+	/**
+	 * Constructeur : cree une ile avec un tableau de parcelles en parametres
+	 * 
+	 * @param Ile
+	 */
+	public Ile(Parcelle[][] tablo) {
+
+		this.ligne = tablo.length;
+		this.colonne = tablo[0].length;
+		this.grille = tablo;
+
+		ileVierge();
+		setElements();
+	}
+
+	/**
+	 * Constructeur : cree une ile avec un tableau de parcelles et un
+	 * pourcentage de rochers en parametres
+	 * 
+	 * @param Ile
+	 */
+	public Ile(Parcelle[][] tablo, int pourcent) {
+
+		this.ligne = tablo.length;
+		this.colonne = tablo[0].length;
+		this.grille = tablo;
+		this.tauxRocher = pourcent * 0.01;
+
+		ileVierge();
+		setElements();
+
+	}
+
+	// Le parametre deplacement est provisoire
+
+	/**
+	 * Déplace un personnage vers une direction précise
+	 * 
+	 * @param e
+	 * @param deplacement
+	 * @return
+	 */
+	public boolean deplacement(Personnage e, String deplacement) {
+		Parcelle tmp = new Sable();
+		switch (deplacement) {
+		case "gauche":
+			if (grille[e.getX()][e.getY() - 1].estTraversablePar(e)) {
+				grille[e.getX()][e.getY() - 1] = grille[e.getX()][e.getY()];
+				grille[e.getX()][e.getY()] = tmp;
+				e.setY(e.getY() - 1);
+				return true;
+			} else if (grille[e.getX()][e.getY() - 1] instanceof Navire) {
+				Navire nav = (Navire) grille[e.getX()][e.getY() - 1];
+				if (nav.getEquipe().getID() == e.getEquipe().getID()) {
+					e.getEquipe().getNavire().getPersoDansNavire().add(e);
+					System.out.println("personnage ajouté.");
+					grille[e.getX()][e.getY()] = tmp;
+					e.setY(e.getY() - 1);
+					return true;
+				} else {
+					System.out.println("Ce n'est pas le navire du personnage.");
+					return false;
+				}
+			}
+			break;
+		case "droite":
+			if (grille[e.getX()][e.getY() + 1].estTraversablePar(e)) {
+				grille[e.getX()][e.getY() + 1] = grille[e.getX()][e.getY()];
+				grille[e.getX()][e.getY()] = tmp;
+				e.setY(e.getY() + 1);
+				return true;
+			} else if (grille[e.getX()][e.getY() + 1] instanceof Navire) {
+				Navire nav = (Navire) grille[e.getX()][e.getY() + 1];
+				if (nav.getEquipe().getID() == e.getEquipe().getID()) {
+					e.getEquipe().getNavire().getPersoDansNavire().add(e);
+					System.out.println("personnage ajouté.");
+					grille[e.getX()][e.getY()] = tmp;
+					e.setY(e.getY() + 1);
+					return true;
+				} else {
+					System.out.println("Ce n'est pas le navire du personnage.");
+					return false;
+				}
+			}
+			break;
+		case "haut":
+			if (grille[e.getX() - 1][e.getY()].estTraversablePar(e)) {
+				grille[e.getX() - 1][e.getY()] = grille[e.getX()][e.getY()];
+				grille[e.getX()][e.getY()] = tmp;
+				e.setX(e.getX() - 1);
+				return true;
+			} else if (grille[e.getX() - 1][e.getY()] instanceof Navire) {
+				Navire nav = (Navire) grille[e.getX() - 1][e.getY()];
+				if (nav.getEquipe().getID() == e.getEquipe().getID()) {
+					e.getEquipe().getNavire().getPersoDansNavire().add(e);
+					System.out.println("personnage ajouté.");
+					grille[e.getX()][e.getY()] = tmp;
+					e.setX(e.getX() - 1);
+					return true;
+				} else {
+					System.out.println("Ce n'est pas le navire du personnage.");
+					return false;
+				}
+			}
+			break;
+		case "bas":
+			if (grille[e.getX() + 1][e.getY()].estTraversablePar(e)) {
+				grille[e.getX() + 1][e.getY()] = grille[e.getX()][e.getY()];
+				grille[e.getX()][e.getY()] = tmp;
+				e.setX(e.getX() + 1);
+				return true;
+			} else if (grille[e.getX() + 1][e.getY()] instanceof Navire) {
+				Navire nav = (Navire) grille[e.getX() + 1][e.getY()];
+				if (nav.getEquipe().getID() == e.getEquipe().getID()) {
+					e.getEquipe().getNavire().getPersoDansNavire().add(e);
+					System.out.println("personnage ajouté.");
+					grille[e.getX()][e.getY()] = tmp;
+					e.setX(e.getX() + 1);
+					return true;
+				} else {
+					System.out.println("Ce n'est pas le navire du personnage.");
+					return false;
+				}
+			}
+			break;
+		default:
+			break;
+		}
+		return false;
+	}
+
+	public boolean debarquement(Personnage e, String deplacement) {
+
+		switch (deplacement) {
+		case "gauche":
+			if (grille[e.getX()][e.getY() - 1].estTraversablePar(e)) {
+				grille[e.getX()][e.getY() - 1] = e;
+				e.setY(e.getY() - 1);
+				e.getEquipe().getNavire().getPersoDansNavire().remove(e);
+				return true;
+			}
+			break;
+		case "droite":
+			if (grille[e.getX()][e.getY() + 1].estTraversablePar(e)) {
+				grille[e.getX()][e.getY() + 1] = e;
+				e.setY(e.getY() + 1);
+				e.getEquipe().getNavire().getPersoDansNavire().remove(e);
+				return true;
+			}
+			break;
+		case "haut":
+			if (grille[e.getX() - 1][e.getY()].estTraversablePar(e)) {
+				grille[e.getX() - 1][e.getY()] = e;
+				e.setX(e.getX() - 1);
+				e.getEquipe().getNavire().getPersoDansNavire().remove(e);
+				return true;
+			}
+			break;
+		case "bas":
+			if (grille[e.getX() + 1][e.getY()].estTraversablePar(e)) {
+				grille[e.getX() + 1][e.getY()] = e;
+				e.setX(e.getX() + 1);
+				e.getEquipe().getNavire().getPersoDansNavire().remove(e);
+				return true;
+			}
+			break;
+		default:
+			break;
+		}
+		return false;
+	}
+
+	// Place deux navires aleatoirement sur des bords opposes de l'ile
+	private void setNavires() {
+
+		Random alea = new Random();
+		this.posNav1 = alea.nextInt(grille.length - 4) + 2;
+		this.posNav2 = alea.nextInt(grille[0].length - 4) + 2;
+
+		Jeu.un.setNavire(new Navire(posNav1, 1));
+		Jeu.deux.setNavire(new Navire(posNav2, grille.length - 2));
+
+		grille[posNav1][1] = Jeu.un.getNavire();
+		grille[posNav2][grille.length - 2] = Jeu.deux.getNavire();
+
+	}
+
+	// Verifie que les parcelles autour des navires sont vides
+	private boolean verifierNavires() {
+
+		if (!(grille[posNav1 + 1][1] instanceof Sable)) {
+			return false;
+		}
+		if (!(grille[posNav1][2] instanceof Sable)) {
+			return false;
+		}
+		if (!(grille[posNav1 - 1][1] instanceof Sable)) {
+			return false;
+		}
+		if (!(grille[posNav2 + 1][grille.length - 2] instanceof Sable)) {
+			return false;
+		}
+		if (!(grille[posNav2 - 1][grille.length - 2] instanceof Sable)) {
+			return false;
+		}
+		if (!(grille[posNav2][grille.length - 3] instanceof Sable)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Retourne la position du navire 1
+	 * 
+	 * @return
+	 */
+	public int getNav1() {
+		return posNav1;
+	}
+
+	/**
+	 * Retourne la position du navire 2
+	 * 
+	 * @return
+	 */
+	public int getNav2() {
+		return posNav2;
+	}
+
+	// Ajoute des rochers en fonction du pourcentage en parametre a la
+	// construction
+	private void setElements() {
+
+		do {
+
+			ileVierge();
+			int nbroc = 0;
+			while (nbroc < getNbRocher()) {
+				Random alea = new Random();
+				int i = alea.nextInt(ligne - 1);
+				int j = alea.nextInt(colonne - 1);
+
+				if (nbroc < getNbRocher() && grille[i][j] instanceof Sable) {
+					grille[i][j] = new Rocher();
+					nbroc++;
+
+				}
+			}
+			setNavires();
+		} while (!verifierIle() && !verifierNavires());
+
+	}
+
+	// Retourne true si tous les rochers sont accessibles, false sinon
+	private boolean verifierIle() {
+		int[][] tablo = new int[ligne][colonne];
+
+		for (int i = 0; i < tablo.length; i++) {
+			for (int j = 0; j < tablo[i].length; j++) {
+
+				if (grille[i][j] instanceof Eau) {
+					tablo[i][j] = 3;
+				} else {
+					tablo[i][j] = 0;
+				}
+			}
+		}
+
+		// Bato
+		tablo[1][1] = 1;
+		grille[1][1].setTraversable(true);
+
+		for (int i = 1; i < tablo.length - 1; i++) {
+
+			for (int j = 1; j < tablo[i].length - 1; j++) {
+
+				if (tablo[i][j] == 1) {
+
+					if (grille[i + 1][j].estTraversable()) {
+						tablo[i + 1][j] = 1;
+					} else {
+						tablo[i + 1][j] = 2;
+					}
+
+					if (grille[i - 1][j].estTraversable()) {
+						tablo[i - 1][j] = 1;
+					} else {
+						tablo[i - 1][j] = 2;
+					}
+
+					if (grille[i][j + 1].estTraversable()) {
+						tablo[i][j + 1] = 1;
+					} else {
+						tablo[i][j + 1] = 2;
+					}
+
+					if (grille[i][j - 1].estTraversable()) {
+						tablo[i][j - 1] = 1;
+					} else {
+						tablo[i][j - 1] = 2;
+					}
+				}
+			}
+		}
+
+		int nbZero = 0;
+
+		for (int i = 0; i < tablo.length; i++) {
+			for (int j = 0; j < tablo[i].length; j++) {
+				if (tablo[i][j] == 0) {
+					nbZero++;
+				}
+			}
+		}
+
+		if (nbZero == 0) {
+			grille[1][1].setTraversable(false);
+			return true;
+		}
+		return false;
+
+	}
+
+	/**
+	 * Retourne le pourcentage de rochers qui doit etre place sur l'ile
+	 * 
+	 * @param getNbRocher
+	 * @return pourcentage de rochers
+	 */
+	public int getNbRocher() {
+		return (int) (ligne * colonne * tauxRocher);
+	}
+
+	/**
+	 * Retourne le nombre de lignes de l'ile
+	 * 
+	 * @param getLigne
+	 * @return nombre de lignes
+	 */
+	public int getLigne() {
+		return ligne;
+	}
+
+	/**
+	 * Retourne le nombre de colonnes de l'ile
+	 * 
+	 * @param getColonne
+	 * @return nombre de colonnes
+	 */
+	public int getColonne() {
+		return colonne;
+	}
+
+	/**
+	 * Retourne la grille
+	 * 
+	 * @param getGrille
+	 * @return grille
+	 */
+	public Parcelle[][] getGrille() {
+		return grille;
+	}
+
+	// Cree une ile de sable et entouree d'eau
+	private void ileVierge() {
+
+		for (int i = 0; i < grille.length; i++) {
+			for (int j = 0; j < grille[i].length; j++) {
+				if (i == 0 || i == grille.length - 1 || j == 0 || j == grille[i].length - 1) {
+					grille[i][j] = new Eau();
+				} else {
+					grille[i][j] = new Sable();
+				}
+			}
+		}
+	}
+
+	/**
+	 * Affiche l'ile Chaque parcelle a son charactere particulier Les
+	 * majuscules/minuscules correspondent aux equipes 1 et 2
+	 * 
+	 * @param toString
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+
+		String retour = "";
+		for (int lig = 1; lig <= ligne * 2 + 1; lig++) {
+			for (int col = 1; col <= colonne * 4 + 1; col++) {
+
+				if (lig % 2 == 1) {
+					if (col % 4 == 1) {
+						retour += "+";
+					} else {
+						retour += "-";
+					}
+				} else if (col % 4 == 1) {
+					retour += "|";
+				} else if (col % 4 == 3) {
+					retour += grille[lig / 2 - 1][col / 4].toString();
+				} else {
+					retour += " ";
+				}
+			}
+			retour += "\n";
+		}
+		return retour;
+	}
+
+}
